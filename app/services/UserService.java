d859f59f390a9b6ff55345b419b041ce4e3eb4e7
Pooja Mahapatra <pooja_mahapatra@apple.com>
2017-03-31 16:48:14 +0530

    Added service to filter the products for each user

--- app/services/UserService.java
+++ app/services/UserService.java
@@ -2,6 +2,7 @@ package services;
 
 import models.IdsAndRules;
 import models.Product;
+import models.RuleIntegration;
 import models.User;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
@@ -10,16 +11,25 @@ import play.Logger;
 import util.MongoConfig;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Pooja Mahapatra on 26/03/17 11:36 AM.
  */
 public class UserService {
 
+
     private Query<User> query = null;
 
-    private User user = new User();
+    private Query<Product> queryProd = null;
+
+    private Product product;
+
+    private Map<Long,List<RuleIntegration>> map;
+
+    private List<Product> userProducts;
 
     public User getUser(String emailId) {
 
@@ -66,36 +76,42 @@ public class UserService {
         User user = query.field("emailId").equal(emailId).retrievedFields(true, "registeredIds").get();
     }
 
-    public List<Product> getFilteredProduct(Long prodId, String operator, String key, Double value) {
-        Query<Product> queryProd = null;
-        List<Product> filteredProducts = null;
-        if (query == null) {
-            queryProd = MongoConfig.getDB().createQuery(Product.class).disableValidation();
-        }
-        if (key.equals("discount")) {
-            Product calcDiscount = queryProd.field("id").equal(prodId).get();
-            Double price = calcDiscount.getPrice();
-            Double discountedPrice = calcDiscount.getDiscountedPrice();
-            Double discount = (price - discountedPrice) / price * 100;
+    public List<Product> getFilteredProduct() {
 
-        } else {
-            queryProd.field("id").equal(prodId);
-            if (operator.equals(">")) {
-                queryProd.field(key).greaterThan(value);
+        final Query<User> queryUser = MongoConfig.getDB().find(User.class);
+        queryUser.fetch().forEach(obj -> {
+            List<IdsAndRules> idsAndRules;
+            map = new HashMap<>();
+            idsAndRules = obj.getIdsAndRules();
+
+            for(IdsAndRules join: idsAndRules){
+                map.put(join.getProdId(),join.getRuleIntegrations());
             }
-            if (operator.equals(">=")) {
-                queryProd.field(key).greaterThanOrEq(value);
+            for(Map.Entry<Long,List<RuleIntegration>> entry: map.entrySet()){
+                userProducts = new ArrayList<>();
+                Long pId = entry.getKey();
+                List<RuleIntegration> ruleIntegration = entry.getValue();
+                for(RuleIntegration rules: ruleIntegration) {
+                    queryProd = MongoConfig.getDB().createQuery(Product.class).disableValidation();
+                    String key = rules.getKey();
+                    String operator = rules.getOperator();
+                    String condition = key+" "+operator;
+                    Double value = rules.getValue();
+                    product = queryProd.field("id").equal(pId).filter(condition,value).get();
+
+                    if(product != null){
+                        userProducts.add(product);
+
+                        //Trigger the mail if filtered products are available
+                        System.out.println(obj.getEmailId());
+                    }
+                }
             }
-            if (operator.equals("<=")) {
-                queryProd.field(key).lessThanOrEq(value);
-            }
-            if (operator.equals("<")) {
-                queryProd.field(key).lessThan(value);
-            }
-            filteredProducts = queryProd.asList();
-        }
-        return filteredProducts;
+        });
+
+        return userProducts;
     }
+
 
     public void unregisterProduct(String emailId, IdsAndRules idsAndRules) {
         query = MongoConfig.getDB().createQuery(User.class).disableValidation();
